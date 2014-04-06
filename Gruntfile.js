51fa34a53d8398b6dcc06a6096dfd3586fb4262d
Gregor Adams <gregor.adams@sinnerschrader.com>
2014-04-06 21:28:00 +0200

    added csslint-test and test for namespaced import

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,12 @@ module.exports = function(grunt) {
                     "test/test.css": "test/test.less"
                 }
             },
+            namespace: {
+                options: {},
+                files: {
+                    "test/namespace.css": "test/namespace.less"
+                }
+            },
             compare: {
                 options: {},
                 files: {
@@ -24,10 +30,29 @@ module.exports = function(grunt) {
                 }
             }
         },
+        csslint: {
+            strict: {
+                options: {
+                    import: 2,
+                    'adjoining-classes': false,
+                    'box-sizing': false,
+                    'box-model': false,
+                    'fallback-colors': false
+                },
+                src: 'public/css/unprefixed-screen.css'
+            }
+        },
         watch: {
             styles: {
                 files: ["**/*.less"],
-                tasks: ["less:development","notify:less"],
+                tasks: ["less:development", "notify:less"],
+                options: {
+                    nospawn: true
+                }
+            },
+            lint: {
+                files: ["**/*.less"],
+                tasks: ["csslint:strict", "notify:csslint"],
                 options: {
                     nospawn: true
                 }
@@ -36,11 +61,13 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-csslint");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-notify");
 
     grunt.registerTask("default", ["less"]);
     grunt.registerTask("dev", ["watch"]);
     grunt.registerTask("compare", ["less:compare"]);
-    grunt.registerTask("test", ["less:development"]);
+    grunt.registerTask("test", ["less:development", "csslint:strict"]);
+    grunt.registerTask("namespace", ["less:namespace"]);
 };
